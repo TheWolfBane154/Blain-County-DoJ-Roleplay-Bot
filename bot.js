f1c9de2b93e4c79320e68db1d8638cf19d38ae83
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2018-12-06 11:24:47 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const fs = require("fs");
+const config = require("./config.json");
+var prefix = "!"; 
+
+fs.readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on("ready", () => {
+console.log(`Hello there! I'm signed  as: ${bot.username} with the ID of: ${bot.username.id}!`)
+});
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    cmd.run(bot, message, args, Discord)
+console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+})
+
+bot.login(process.env.Token)
