1710dad7b5383b1f7f98ce5cb45d8c662856fbb6
TheWolfBane154 <45442131+TheWolfBane154@users.noreply.github.com>
2018-12-08 20:35:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const fs = require("fs");
 const config = require("./config.json");
-var prefix = "!"; 
+var prefix = "."; 
 
 bot.commands = new Discord.Collection()
 
